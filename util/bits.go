70a5850428e73885e667fc3a84700d3d217896c9
Jason Harris <sirmanlypowers@gmail.com>
2019-12-20 11:02:43 -0800

    Added bit field decode/display utilities.

--- util/bits.go
+++ util/bits.go
@@ -16,14 +16,17 @@ func bitMask(msb, lsb uint) uint {
 	return ((1 << n) - 1) << lsb
 }
 
+// RdBits reads a bit field from a value.
 func RdBits(x, msb, lsb uint) uint {
 	return (x & bitMask(msb, lsb)) >> lsb
 }
 
+// MaskBits masks a bit field within a value.
 func MaskBits(x, msb, lsb uint) uint {
 	return x & bitMask(msb, lsb)
 }
 
+// WrBits writes a bit field within a value.
 func WrBits(x, val, msb, lsb uint) uint {
 	mask := bitMask(msb, lsb)
 	val = (val << lsb) & mask
