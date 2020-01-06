86d491abcad32252884e6f628400c4ffe45d34f5
Jason Harris <sirmanlypowers@gmail.com>
2020-01-06 11:22:35 -0800

    Added a host interface object.

---
+++ host/host.go
@@ -0,0 +1,59 @@
+//-----------------------------------------------------------------------------
+/*
+
+to/from host functions
+
+Compliance testing communicates with the "host" via reads/writes to memory
+locations with symbols "tohost" and "fromhost". This code intercepts this
+acess via the breakpoint mechanism and records interesting state.
+
+*/
+//-----------------------------------------------------------------------------
+
+package host
+
+import "github.com/deadsy/riscv/mem"
+
+//-----------------------------------------------------------------------------
+
+type Host struct {
+	addr uint   // base address of "tohost" symbol
+	text []rune // characters written "tohost"
+}
+
+func NewHost(m *mem.Memory) *Host {
+	sym := m.SymbolByName("tohost")
+	if sym == nil {
+		return nil
+	}
+	return &Host{
+		addr: sym.Addr,
+		text: make([]rune, 0, 128),
+	}
+}
+
+// The risc-v compliance tests use this as the upper word in "tohost" when
+// they want to send a character to the host.
+const charMagic = 0x01010000
+
+// To intercepts writes to the tohost memory location.
+func (h *Host) To(m *mem.Memory, bp *mem.BreakPoint) bool {
+	// break by default unless we consume the write
+	brk := true
+	val, _ := m.Rd64Phys(h.addr)
+	// Is this a character write?
+	if (val >> 32) == charMagic {
+		h.text = append(h.text, rune(val&0xff))
+		// signal write consumption to the risc-v test.
+		m.Wr64Phys(h.addr, 0)
+		brk = false
+	}
+	return brk
+}
+
+// GetText returns the string of characters written "tohost".
+func (h *Host) GetText() string {
+	return string(h.text)
+}
+
+//-----------------------------------------------------------------------------
